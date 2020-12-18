842b60f09b2e3f407889a09d536b743b91bcdf6d
Yelaw3 <62616783+Yelaw3@users.noreply.github.com>
2020-12-18 23:40:15 +0100

    main menu
    background image
    main menu buttons

--- RabbitUnityProject/Assets/Scripts/MainMenu.cs
+++ RabbitUnityProject/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
+
+
 }
