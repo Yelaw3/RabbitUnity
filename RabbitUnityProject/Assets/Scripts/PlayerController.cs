fb4b12d918f124f3f093469f3f2331dcfc456d75
Yelaw3 <62616783+Yelaw3@users.noreply.github.com>
2020-12-18 18:49:07 +0100

    the whole game
    basically i forgot to push on git, so it's all the changes i've done since i started the game.

---
+++ RabbitUnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float turnSpeed = 15;
+    private float horizontalInput;
+    private float forwardInput;
+    public float jumpForce = 10;
+    public float speed = -4;
+    public AudioClip jumpSound;
+    private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.back * speed * forwardInput * Time.deltaTime);
+        transform.Rotate(transform.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerAnim.SetTrigger("jump_trigger");
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAudio.PlayOneShot(jumpSound, 0.5f);
+        }
+        else playerAnim.SetBool("jump_boolean", false);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+}
