4eafba2ef783b964b5112fb810f86071240da284
Yelaw3 <62616783+Yelaw3@users.noreply.github.com>
2020-12-27 23:43:41 +0100

    new animations/UI

--- RabbitUnityProject/Assets/Scripts/CollectibleScript.cs
+++ RabbitUnityProject/Assets/Scripts/CollectibleScript.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class CollectibleScript : MonoBehaviour
 {
+    public float rotateSpeed = 30f;
+    public GameObject collectEffect;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * rotateSpeed);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             other.GetComponent<ScoreText>().points++;
+            Instantiate(collectEffect, transform.position, transform.rotation);
             Destroy(gameObject);
+
         }
     }
 }
---
+++ RabbitUnityProject/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float time = 5f;
+    public GameObject CollectCarrotsText;
+
+    void Start()
+    {
+        CollectCarrotsText.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, time);
+    }
+}
